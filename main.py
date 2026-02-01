f61175cbcbc4635448bb2215cc4b0c58f7a7afef
shamalkale2007-jpg <shamalkale2007@gmail.com>
2026-02-01 15:22:06 +0530

    Add personalized greeting and coder details
    Updated coder information and added personalized greeting.

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 # AIM: Create a personalized greeting message
 #      User enters name: eg: Shibu. and the program should print "Hello, Shibu" 
-# Coder: 
-# Date:
+# Coder: Shamal
+# Date:26/01/2026
 
 # Write your code here
+name = input("Dhanshree ")
+age = input(33)
+
+print("\nWelcome to Rizvi College of Engineering")
+print(f"Hello, {Dhanshree}")
+print(f"You are {33} years old.")
